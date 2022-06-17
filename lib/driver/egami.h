643eab0db3ac967a8a15cda0713edadcca99d9bd
RedouaneTM@ <redouane343@yahoo.com>
2022-06-17 18:07:39 +0100

    Update egami.h

--- lib/driver/egami.h
+++ lib/driver/egami.h
@@ -23,7 +23,7 @@ public:
 	bool endsWith(const char* base, const char* str);
 	bool startsWith(const char* base, const char* str);
 	//char* ReadProcEntry(const char *filename);
-	bool checkkernel() { return 0; };
+	bool checkkernel();
 };
 
 #endif
